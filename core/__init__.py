17817fc654c6d6ffc9221ed61d76c72bac784f92
eco <spambaconspam@protonmail.com>
2024-12-03 20:20:51 +0100

    started implementing cademlia

--- core/__init__.py
+++ core/__init__.py
@@ -62,11 +62,13 @@ def parse_args(state):
     test = subparsers.add_parser("test", help="build script")
     test.add_argument('-p', '--peers',  help='amount of peers', metavar="AMOUNT", type=int, required=True)
     test.add_argument('-n', '--net-size',  help='size of network in bits', metavar="SIZE", type=int, default=16)
+    test.add_argument('-k', '--bucket-size',  help='amount of peers per bucket', metavar="SIZE", type=int, default=5)
     test.add_argument('-D', '--debug', help='enable debugging', action='store_true')
 
     run = subparsers.add_parser("run", help="build and run script")
     run.add_argument('-p', '--port',  help='listen port', metavar="PORT", type=int, default=666)
     run.add_argument('-u', '--uuid',  help='uuid', metavar="UUID", type=int)
+    run.add_argument('-k', '--bucket-size',  help='amount of peers per bucket', metavar="SIZE", type=int, default=5)
     run.add_argument('-n', '--net-size',  help='size of network in bits', metavar="SIZE", type=int, default=16)
     run.add_argument('-D', '--debug', help='enable debugging', action='store_true')
 
@@ -76,15 +78,17 @@ def parse_args(state):
     state.do_run = False
 
     if args.command == "test":
-        state.do_test = True
-        state.peers = args.peers
-        state.net_size = args.net_size
+        state.do_test     = True
+        state.peers       = args.peers
+        state.bucket_size = args.bucket_size
+        state.net_size    = args.net_size
 
     elif args.command == "run":
-        state.do_run   = True
-        state.port     = args.port
-        state.net_size = args.net_size
-        state.uuid = args.uuid
+        state.do_run      = True
+        state.port        = args.port
+        state.net_size    = args.net_size
+        state.bucket_size = args.bucket_size
+        state.uuid        = args.uuid
     else:
         parser.print_help()
         sys.exit()
--- p2p/node.py
+++ p2p/node.py
@@ -17,7 +17,6 @@ class Peer():
     # An n-bit identifier that represents the peer's location in the binary tree.
     # An XOR(origin, peer) is done to calculate the distance.
     uuid: int
-
     ip: str
     port: int
 
@@ -27,144 +26,89 @@ class Peer():
     def get_distance(self, origin):
         return self.uuid ^ origin
 
+    def find_significant_common_bits(self, origin: int, size: int) -> int:
+        """ Compare uuid's and find common most significant bits.
+            This is used to determin the bucket we should store this peer """
+
+        # get the non matching bits between peer and origin node
+        distance = self.get_distance(origin)
+        count = 0
+
+        for i in reversed(range(size)):
+            if distance & (0x01 << i):
+                break
+            else:
+                count += 1
+        return count
+
 
 class RouteTable():
-    def __init__(self, table_size: int, uuid: int) -> None:
-        self._table_size = table_size
-        self._table: list[Optional[Peer]] = [None] * table_size
+    def __init__(self, key_size: int, bucket_size: int) -> None:
+        self._n_buckets = key_size
+        self._bucket_size = bucket_size
 
-        # The uuid of this node.
-        # Since the routing table represents the distance from this node we need to calculate the offset
-        self._uuid = uuid
+        """
+        The buckets where we store peers based on most significant common bit.
+        This means that peers are stored based on distance from origin, NOT based
+        on just the uuid.
 
-    def __repr__(self):
-        out = [(f"ROUTE_TABLE ({2**self._table_size})")]
+        8 BIT table
+        peer_uuid   = 0b01000100
+        origin_uuid = 0b01011100
+                      ---------- XOR
+        common      = 0b00011000
+                           ^
+        This example has 3 msb's.
+        This means that we have 3 matching steps down the binary tree.
+        We put this peer in bucket 3.
 
-        uuids = ["UUID"]
-        uuids += [str((self._uuid + 2**i) % 2**self._table_size) for i in range(0, self._table_size)]
-        uuids_rjust = max(len(x) for x in uuids)
-        uuids = [x.rjust(uuids_rjust) for x in uuids]
-
-
-        ids = ["INDEX"]
-        ids += [str(x) for x in range(self._table_size)]
-        i_rjust = max(len(x) for x in ids)
-        ids = [x.rjust(i_rjust) for x in ids]
-
-        peers = ["PEERS"]
-        peers += self._table
-
-        for uuid, id, peer in zip(uuids, ids, peers):
-            out.append(f"{id}  {uuid}  {peer}")
-
-        return "\n".join(out)
-
-    def get_distance(self, uuid: int):
-        """ Calculate distance of this node to UUID """
-        assert(uuid != self._uuid)
-
-        # Node is ahead of us in the ring
-        if uuid > self._uuid:
-            return uuid - self._uuid
-
-        # Node is behind us in the ring
-        else:
-            return uuid + (2**self._table_size) - self._uuid
-
-    def distance_to_pos(self, distance: int):
-        """ Convert distance to index of node in routing table """
-        return int(math.log2(distance))
-
-    def get_nearest_node(self, uuid: int) -> Optional[Peer]:
-        """ Return known node that's nearest to requested node """
-
-        # Search in a ring only goes forward starting from the nodes own UUID.
-        # So closest node to 35 would be at position 2**5=32
-        # Calculate in which bit the node is stored that is closest to the node we're looking for
-        distance = self.get_distance(uuid)
-        pos = self.distance_to_pos(distance)
-        return self._table[pos]
-class RouteTable():
-    def __init__(self, table_size: int, uuid: int) -> None:
-        self._table_size = table_size
-        self._table: list[Optional[Peer]] = [None] * table_size
-
-        # The uuid of this node.
-        # Since the routing table represents the distance from this node we need to calculate the offset
-        self._uuid = uuid
+        The bucket size (how many peers can we store in a bucket before it's full)
+        is called K in the spec.
+        """
+        self._k_buckets: list[list] = [ [] for _ in range(self._n_buckets) ]
 
     def __repr__(self):
-        out = [(f"ROUTE_TABLE ({2**self._table_size})")]
-
-        uuids = ["UUID"]
-        uuids += [str((self._uuid + 2**i) % 2**self._table_size) for i in range(0, self._table_size)]
-        uuids_rjust = max(len(x) for x in uuids)
-        uuids = [x.rjust(uuids_rjust) for x in uuids]
-
-
-        ids = ["INDEX"]
-        ids += [str(x) for x in range(self._table_size)]
-        i_rjust = max(len(x) for x in ids)
-        ids = [x.rjust(i_rjust) for x in ids]
-
-        peers = ["PEERS"]
-        peers += self._table
-
-        for uuid, id, peer in zip(uuids, ids, peers):
-            out.append(f"{id}  {uuid}  {peer}")
-
+        out = []
+        for i_bucket, bucket in enumerate(self._k_buckets):
+            out.append(f"BUCKET: {i_bucket}:")
+            for i_peer, peer in enumerate(bucket):
+                out.append(f"  {i_peer}: {str(peer)}")
         return "\n".join(out)
 
-    def get_distance(self, uuid: int):
-        """ Calculate distance of this node to UUID """
-        assert(uuid != self._uuid)
+    def insert_peer(self, peer: Peer, origin: int):
+        if (n := peer.find_significant_common_bits(origin, self._n_buckets)) == None:
+            error("route_table", "init", f"no common bits")
+            return
 
-        # Node is ahead of us in the ring
-        if uuid > self._uuid:
-            return uuid - self._uuid
+        # If max size for bucket is reached keep the old (stable) ones
+        # TODO: we need to check the nodes in the bucket sometimes to check if they're still alive
+        if len(self._k_buckets[n]) >= self._bucket_size:
+            info("route_table", "insert", f"not inserting, bucket full")
+            return
 
-        # Node is behind us in the ring
-        else:
-            return uuid + (2**self._table_size) - self._uuid
-
-    def distance_to_pos(self, distance: int):
-        """ Convert distance to index of node in routing table """
-        return int(math.log2(distance))
-
-    def get_nearest_node(self, uuid: int) -> Optional[Peer]:
-        """ Return known node that's nearest to requested node """
-
-        # Search in a ring only goes forward starting from the nodes own UUID.
-        # So closest node to 35 would be at position 2**5=32
-        # Calculate in which bit the node is stored that is closest to the node we're looking for
-        distance = self.get_distance(uuid)
-        pos = self.distance_to_pos(distance)
-        return self._table[pos]
-
-
-
-
-
+        self._k_buckets[n].append(peer)
 
 
 class Node():
-    def __init__(self, ip: str, port: int, net_size: int, uuid: Optional[int]=None) -> None:
+    def __init__(self, ip: str, port: int, key_size: int, bucket_size: int, uuid: Optional[int]=None) -> None:
 
-        # size of network in bits
-        self._net_size = net_size
+        # Size of network uuid/keys in bits
+        self._key_size = key_size
+
+        # Amount of peers stored per bucket
+        self._bucket_size = bucket_size
 
         self._ip = ip
         self._port = port
+
+        print(">>>uuid:",uuid)
 
         if uuid == None:
             self._uuid = self._get_uuid()
         else:
             self._uuid = uuid
 
-        self._route_table = RouteTable(self._net_size, self._uuid)
-
-        print(self._route_table)
-        print(self._route_table.get_nearest_node(1))
+        self._table = RouteTable(key_size, bucket_size)
 
         self._is_stopped = False
 
@@ -172,38 +116,17 @@ class Node():
         return f"NODE: {self._ip}:{self._port} => {self._uuid}"
 
     def _get_uuid(self):
-        return random.randrange(0, (2**self._net_size)-1)
+        return random.randrange(0, (2**self._key_size)-1)
 
     def stop(self):
         self._is_stopped = True
 
-    def http_get(self, url: str, timeout: int=5):
-        try:
-            r = requests.get(url, timeout=timeout)
-            r.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            print(e)
-            return
-        except requests.ConnectionError as e:
-            print(e)
-            return
-        except requests.Timeout as e:
-            print(e)
-            return
-
-        return r.json()
-
-    def search_peer(self, peer: Peer, uuid: int, timeout=3):
-        """ Contact peer to ask for route to uuid """
-        if js := self.http_get(f"{peer.ip}:{peer.port}/find_peer/{uuid}"):
-            print(f"response: {peer}")
-            print(json.dumps(js, indent=4))
-        else:
-            print(f"Failed to find peer: {peer}")
-
     def run(self):
+        for _ in range(2000):
+            n = random.randrange(0, 2**self._key_size-1)
+            self._table.insert_peer(Peer(n, "127.0.0.1", n), self._uuid)
 
-
+        print(self._table)
 
         try:
             api = Api("", self._port)
