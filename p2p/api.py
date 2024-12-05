f492813ee2727e5ad688b0573ad0ff77565b2f62
eco <spambaconspam@protonmail.com>
2024-12-05 09:12:48 +0100

    replaced rpc keys for strenums

--- p2p/api.py
+++ p2p/api.py
@@ -6,14 +6,44 @@ import select
 import json
 import random
 import string
+from enum import Enum, StrEnum
 
 from p2p.api_parsers import Bencoder, BencDecodeError
 from core.utils import debug, info, error
 
-ERROR_GENERIC  = 201  # the rest of the errors
-ERROR_SERVER   = 202  # internal server error
-ERROR_PROTOCOL = 203  # malformed packet, invalid arguments or bad token
-ERROR_METHOD   = 204  # unexpected method, eg: from api we only reply to query messages
+class MsgErrorCode(Enum):
+    GENERIC   = 201  # the rest of the errors
+    SERVER    = 202  # internal server error
+    PROTOCOL  = 203  # malformed packet, invalid arguments or bad token
+    METHOD    = 204  # unexpected method, eg: from api we only reply to query messages
+    UNDEFINED = -1
+
+class MsgKey(StrEnum):
+    TRANSACTION_ID = "t"   # required nonce
+    MSG_TYPE       = "y"   # v is specified in MsgType()
+    QUERY_TYPE     = "q"   # v is specified in MsgQueryType()
+    QUERY_ARGS     = "a"   # query messages have required arguments with sender id
+    RESPONSE_ARGS  = "r"   # response messages have required arguments with sender id
+    ERROR_ARGS     = "e"   # error messages have required args with code and message
+
+# Used in query message as sender identifier
+# Used in find_node query to list found peers and target peer
+# Used in response message as sender identifier
+class MsgIdKey(StrEnum):
+    UUID = "uuid"
+    IP   = "ip"
+    PORT = "port"
+
+class MsgType(StrEnum):
+    QUERY    = "q"
+    RESPONSE = "r"
+    ERROR    = "e"
+
+class MsgQueryType(StrEnum):
+    PING      = "ping"
+    STORE     = "store"
+    FIND_NODE = "find_node"
+    FIND_KEY  = "find_key"
 
 class MsgError(Exception): pass
 
@@ -35,28 +65,20 @@ class MsgBaseClass(Bencoder):
 
     @property
     def transaction_id(self):
-        return self._data["t"]
+        return self._data[MsgKey.TRANSACTION_ID]
 
     @transaction_id.setter
     def transaction_id(self, transaction_id: str):
-        self._data["t"] = transaction_id
-
-    @property
-    def msg_type(self):
-        return self._data["y"]
-
-    @msg_type.setter
-    def msg_type(self, mtype: str):
-        self._data["y"] = mtype
+        self._data[MsgKey.TRANSACTION_ID] = transaction_id
 
     def is_query(self):
-        return self._data.get("y") == "q"
+        return self._data.get(MsgKey.MSG_TYPE) == MsgType.QUERY
 
     def is_response(self):
-        return self._data.get("y") == "r"
+        return self._data.get(MsgKey.MSG_TYPE) == MsgType.RESPONSE
 
     def is_error(self):
-        return self._data.get("y") == "e"
+        return self._data.get(MsgKey.MSG_TYPE) == MsgType.ERROR
 
     def from_bencoding(self, data: str):
         self._data = self.loads(data)
@@ -83,36 +105,34 @@ class ResponseMsg(MsgBaseClass):
         if all([uuid, ip, port]):
             self.set_sender_node(uuid, ip, port)
 
-        self.msg_type = "r"
-
     @property
     def id(self):
-        return self._data["a"]["id"]
+        return self._data[MsgKey.QUERY_ARGS]["id"]
 
     def set_sender_node(self, uuid: int, ip: str, port: int):
-        if not "r" in self._data.keys():
-            self._data["r"] = {}
-        self._data["r"]["id"] = { "uuid": uuid,
-                                  "ip":   ip,
-                                  "port": port }
+        if not MsgKey.RESPONSE_ARGS in self._data.keys():
+            self._data[MsgKey.RESPONSE_ARGS] = {}
+        self._data[MsgKey.RESPONSE_ARGS]["id"] = { MsgIdKey.UUID: uuid,
+                                                   MsgIdKey.IP:   ip,
+                                                   MsgIdKey.PORT: port }
     @property
     def return_values(self):
-        return self._data["r"]
+        return self._data[MsgKey.RESPONSE_ARGS]
 
     @return_values.setter
     def return_values(self, data: dict):
-        self._data["r"] |= data
+        self._data[MsgKey.RESPONSE_ARGS] |= data
 
     def validate(self):
         if not self._data.get("t"):
             raise MsgError(f"Failed to validate message, message has no transaction id")
-        if not self._data.get("y"):
+        if not self._data.get(MsgKey.MSG_TYPE):
             raise MsgError(f"Failed to validate message, message has no type information")
-        if not self._data["y"] == "r":
+        if not self._data["y"] == MsgType.RESPONSE:
             raise MsgError(f"Failed to validate message, message type is not response")
-        if not self._data.get("r"):
+        if not self._data.get(MsgKey.RESPONSE_ARGS):
             raise MsgError(f"Failed to validate response message, message has no response type information")
-        if not self._data["r"].get("id"):
+        if not self._data[MsgKey.RESPONSE_ARGS].get("id"):
             raise MsgError(f"Failed to validate response message, id key not found in arguments")
 
 
@@ -128,138 +148,130 @@ class QueryMsgBaseClass(MsgBaseClass):
         if all([uuid, ip, port]):
             self.set_sender_node(uuid, ip, port)
 
-        self.msg_type = "q"
-
     @property
     def query_type(self):
-        return self._data["q"]
+        return self._data[MsgKey.QUERY_TYPE]
 
     @query_type.setter
     def query_type(self, qtype: str):
-        self._data["q"] = qtype
+        self._data[MsgKey.QUERY_TYPE] = qtype
 
     @property
     def id(self):
-        return self._data["a"]["id"]
+        return self._data[MsgKey.QUERY_ARGS]["id"]
 
     def set_sender_node(self, uuid: int, ip: str, port: int):
-        if not "a" in self._data.keys():
-            self._data["a"] = {}
-        self._data["a"]["id"] = { "uuid": uuid,
-                                  "ip":   ip,
-                                  "port": port }
+        if not MsgKey.QUERY_ARGS in self._data.keys():
+            self._data[MsgKey.QUERY_ARGS] = {}
+        self._data[MsgKey.QUERY_ARGS]["id"] = { MsgIdKey.UUID: uuid,
+                                                MsgIdKey.IP:   ip,
+                                                MsgIdKey.PORT: port }
 
     def validate(self):
         print("validating:", self._data)
-        if not self._data.get("t"):
+        if not self._data.get(MsgKey.TRANSACTION_ID):
             raise MsgError(f"Failed to validate message, message has no transaction id")
-        if not self._data.get("y"):
+        if not self._data.get(MsgKey.MSG_TYPE):
             raise MsgError(f"Failed to validate message, message has no type information")
-        if not self._data["y"] == "q":
+        if not self._data[MsgKey.MSG_TYPE] == MsgType.QUERY:
             raise MsgError(f"Failed to validate message, message type is not query")
-        if not self._data.get("q"):
+        if not self._data.get(MsgKey.QUERY_TYPE):
             raise MsgError(f"Failed to validate query message, message has no query type information")
-        if not self._data.get("a"):
+        if not self._data.get(MsgKey.QUERY_ARGS):
             raise MsgError(f"Failed to validate query message, message has no arguments")
-        if not self._data["a"].get("id"):
+        if not self._data[MsgKey.QUERY_ARGS].get("id"):
             raise MsgError(f"Failed to validate query message, id dictionary not found in arguments")
-        if not self._data["a"]["id"].get("uuid"):
+        if not self._data[MsgKey.QUERY_ARGS]["id"].get(MsgIdKey.UUID):
             raise MsgError(f"Failed to validate query message, missing uuid in id")
-        if not self._data["a"]["id"].get("ip"):
+        if not self._data[MsgKey.QUERY_ARGS]["id"].get(MsgIdKey.IP):
             raise MsgError(f"Failed to validate query message, missing ip in id")
-        if not self._data["a"]["id"].get("port"):
+        if not self._data[MsgKey.QUERY_ARGS]["id"].get(MsgIdKey.PORT):
             raise MsgError(f"Failed to validate query message, missing port in id")
 
-        match self._data["q"]:
-            case "ping":
+        match self._data[MsgKey.QUERY_TYPE]:
+            case MsgQueryType.PING:
                 # ping has no extra arguments
                 ...
-            case "store":
+            case MsgQueryType.STORE:
                 ...
-            case "find_node":
-                if not self._data["a"].get("target"):
+            case MsgQueryType.FIND_NODE:
+                if not self._data[MsgKey.QUERY_ARGS].get("target"):
                     raise MsgError(f"Failed to validate find_node query message, target node not found in arguments")
-            case "find_key":
+            case MsgQueryType.FIND_KEY:
                 ...
             case _:
-                raise MsgError(f"Failed to validate message, unknown query type: {self._data['y']}")
+                raise MsgError(f"Failed to validate message, unknown query type: {self._data[MsgKey.QUERY_TYPE]}")
 
 
 class PingMsg(QueryMsgBaseClass):
     def __init__(self, *args, **kwargs):
         QueryMsgBaseClass.__init__(self, *args, **kwargs)
-        self.query_type = "ping"
 
 
 class StoreMsg(QueryMsgBaseClass):
     def __init__(self, *args, **kwargs):
         QueryMsgBaseClass.__init__(self, *args, **kwargs)
-        self.query_type = "store"
 
 
 class FindNodeMsg(QueryMsgBaseClass):
     def __init__(self, *args, **kwargs):
         QueryMsgBaseClass.__init__(self, *args, **kwargs)
-        self.query_type = "find_node"
 
     @property
     def target_node(self):
-        return self._data["a"].get("target")
+        return self._data[MsgKey.QUERY_ARGS].get("target")
 
     def set_target_node(self, uuid: int, ip: str, port: int):
-        self._data["a"]["target"] = { "uuid": uuid,
-                                      "ip":   ip,
-                                      "port": port }
+        self._data[MsgKey.QUERY_ARGS]["target"] = { MsgIdKey.UUID: uuid,
+                                                    MsgIdKey.IP:  ip,
+                                                    MsgIdKey.PORT: port }
 
 
 class FindKeyMsg(QueryMsgBaseClass):
     def __init__(self, *args, **kwargs):
         QueryMsgBaseClass.__init__(self, *args, **kwargs)
-        self.query_type = "find_key"
 
 
 class ErrorMsg(MsgBaseClass):
     def __init__(self, *args, code: Optional[int]=None, msg: Optional[str]=None, **kwargs):
         MsgBaseClass.__init__(self, *args, **kwargs)
         self._error_code = code
-        self._data["e"] = [None, None]
+        self._data[MsgKey.ERROR_ARGS] = [None, None]
 
         if code:
             self.error_code = code
         if msg:
             self.error_msg = msg
 
-        self.msg_type = "e"
-
     @property
     def error_code(self):
-        return self._data["e"][0]
+        return self._data[MsgKey.ERROR_ARGS][0]
 
     @error_code.setter
     def error_code(self, code: int):
-        self._data["e"][0] = code
+        self._data[MsgKey.ERROR_ARGS][0] = code
 
     @property
     def error_msg(self):
-        return self._data["e"][1]
+        return self._data[MsgKey.ERROR_ARGS][1]
 
     @error_msg.setter
     def error_msg(self, msg: str):
-        self._data["e"][1] = msg
+        self._data[MsgKey.ERROR_ARGS][1] = msg
 
     def validate(self):
-        if not self._data.get("t"):
+        if not self._data.get(MsgKey.TRANSACTION_ID):
             raise MsgError(f"Failed to validate message, message has no transaction id")
-        if not self._data.get("y"):
+        if not self._data.get(MsgKey.MSG_TYPE):
             raise MsgError(f"Failed to validate message, message has no type information")
-        if not self._data["y"] == "e":
+        if not self._data[MsgKey.MSG_TYPE] == MsgType.ERROR:
             raise MsgError(f"Failed to validate message, message type is not error")
-        if not self._data.get("e"):
+        if not self._data.get(MsgKey.ERROR_ARGS):
             raise MsgError(f"Failed to validate error message, message has no error information")
 
         try:
-            int(self._data.get("e")[0])   # error code
-            self._data.get("e")[1]        # error message
+            int(self._data.get(MsgKey.ERROR_ARGS)[0])   # error code
+            self._data.get(MsgKey.ERROR_ARGS)[1]        # error message
         except IndexError:
             raise MsgError(f"Failed to validate error message, message has malformed error")
         except ValueError:
@@ -308,40 +320,40 @@ class ConnThread(threading.Thread):
             try:
                 parsed = Bencoder().loads(data.decode())
             except BencDecodeError as e:
-                self.send(ErrorMsg(code=ERROR_PROTOCOL, msg=str(e)))
+                self.send(ErrorMsg(code=MsgErrorCode.PROTOCOL, msg=str(e)))
                 error("conn_thread", str(self), f"{e}")
                 return
 
             print(json.dumps(parsed, indent=4))
 
-            if parsed.get("y") == None:
-                self.send(ErrorMsg(code=ERROR_PROTOCOL, msg="missing msg type"))
+            if parsed.get(MsgKey.MSG_TYPE) == None:
+                self.send(ErrorMsg(code=MsgErrorCode.PROTOCOL, msg="missing msg type"))
                 error("conn_thread", str(self), "Missing message type")
                 return
 
-            if (qtype := parsed.get("q")) == None:
-                self.send(ErrorMsg(code=ERROR_PROTOCOL, msg="missing query type"))
+            if (qtype := parsed.get(MsgType.QUERY)) == None:
+                self.send(ErrorMsg(code=MsgErrorCode.PROTOCOL, msg="missing query type"))
                 error("conn_thread", str(self), "Missing query type")
                 return
 
             match qtype:
-                case "ping":
+                case MsgQueryType.PING:
                     msg = PingMsg()
-                case "store":
+                case MsgQueryType.STORE:
                     msg = StoreMsg()
-                case "find_node":
+                case MsgQueryType.FIND_NODE:
                     msg = FindNodeMsg()
-                case "find_key":
+                case MsgQueryType.FIND_KEY:
                     msg = FindKeyMsg()
                 case _:
-                    self.send(ErrorMsg(code=ERROR_METHOD, msg="unknown query type"))
+                    self.send(ErrorMsg(code=MsgErrorCode.METHOD, msg="unknown query type"))
                     error("conn_thread", str(self), "Unknown query type")
                     return
 
             try:
                 msg.from_dict(parsed)
             except MsgError as e:
-                self.send(ErrorMsg(code=ERROR_PROTOCOL, msg=str(e)))
+                self.send(ErrorMsg(code=MsgErrorCode.PROTOCOL, msg=str(e)))
                 error("conn_thread", str(self), str(e))
                 return
 
